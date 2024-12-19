16806da64d858387304f0eb40247026260961923
yangzhouQS <2638497222@qq.com>
2024-12-19 14:11:50 +0800

    fix: 底部tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tdesign_flutter/tdesign_flutter.dart';
+import './page-button.dart';
+import './page-layout.dart';
 
 /**
  * 应用入口
@@ -19,27 +21,16 @@ class MyApp extends StatelessWidget {
       title: '基础设施测试应用',
       theme: ThemeData(
         primarySwatch: Colors.amber,
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
 
+      routes: {
+        "pageButton": (BuildContext context) => new PageButton(),
+        "pageLayout": (BuildContext context) => new PageLayout(),
+      },
+
       // 应用首页路由
-      home: const MyHomePage(title: '本地测试页面'),
+      home: const MyHomePage(title: '移动收发'),
     );
   }
 }
@@ -49,15 +40,6 @@ class MyApp extends StatelessWidget {
  */
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -70,117 +52,44 @@ class MyHomePage extends StatefulWidget {
  */
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _selectedIndex = 1;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter += 10;
     });
   }
 
+  final List<BottomNavigationBarItem> bottomNavItems = [
+    new BottomNavigationBarItem(
+      backgroundColor: Colors.blue,
+      icon: Icon(Icons.home),
+      label: '首页',
+    ),
+    new BottomNavigationBarItem(
+      backgroundColor: Colors.green,
+      icon: Icon(Icons.message),
+      label: '消息',
+    ),
+    new BottomNavigationBarItem(
+      backgroundColor: Colors.amber,
+      icon: Icon(Icons.verified_user),
+      label: '我的',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const TDButton(
-              text: '填充按钮',
-              size: TDButtonSize.large,
-              type: TDButtonType.fill,
-              shape: TDButtonShape.rectangle,
-              theme: TDButtonTheme.light,
-            ),
-            TDButton(
-              text: '填充按钮',
-              size: TDButtonSize.large,
-              type: TDButtonType.fill,
-              shape: TDButtonShape.rectangle,
-              theme: TDButtonTheme.defaultTheme,
-            ),
-            ElevatedButton(onPressed: () {}, child: const Text("普通按钮")),
-            TextButton(onPressed: () {}, child: const Text("文本按钮")),
-            OutlinedButton(onPressed: () {}, child: const Text("边框按钮")),
-            IconButton(icon: Icon(Icons.thumb_up), onPressed: () {}),
-            ElevatedButton.icon(
-              icon: Icon(Icons.send),
-              label: Text("发送"),
-              onPressed: () {},
-            ),
-            OutlinedButton.icon(
-              icon: Icon(Icons.add),
-              label: Text("添加"),
-              onPressed: () {},
-            ),
-            TextButton.icon(
-              icon: Icon(Icons.info),
-              label: Text("详情"),
-              onPressed: () {},
-            ),
-            TextButton(
-
-                // 回调函数，当按钮被点击时调用
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        // 回调函数返回新路由的实例
-                        builder: (context) => NewRoute(),
-                        // 包含路由的配置信息，如路由名称、是否初始路由（首页）
-                        settings: RouteSettings(name: "new route"),
-
-                        // 默认情况下，当入栈一个新路由时，原来的路由仍然会被保存在内存中，如果想在路由没用的时候释放其所占用的所有资源，可以设置maintainState为 false。
-                        maintainState: false,
-
-                        // 是否是全屏的弹出路由，默认是false
-                        fullscreenDialog: true),
-                  );
-                },
-                child: const Text("open 全屏 新页面")),
-            const Text(
-              '这是一个累加器:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      bottomNavigationBar: new BottomNavigationBar(
+        items: bottomNavItems,
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
@@ -189,6 +98,14 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onAdd() {}
 }
 
 class NewRoute extends StatelessWidget {
---
+++ lib/page-button.dart
@@ -0,0 +1,14 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class PageButton extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
