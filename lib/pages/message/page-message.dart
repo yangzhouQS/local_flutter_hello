7e0af15e4687c370d4aa61126ad38aec40da04fb
yangzhouQS <2638497222@qq.com>
2024-12-20 15:54:14 +0800

    fix: tag组件

--- lib/pages/message/page-message.dart
+++ lib/pages/message/page-message.dart
@@ -9,13 +9,112 @@ class PageMessage extends StatefulWidget {
 }
 
 class _PageMessageState extends State<PageMessage> {
-
-
   @override
   Widget build(BuildContext context) {
-
-    return Container(
-      child: Text('message'),
+    return Column(
+      children: [
+        Wrap(spacing: 8, children: [
+          TDTag('默认', isLight: true),
+          TDTag(
+            '主要',
+            isLight: true,
+            theme: TDTagTheme.primary,
+          ),
+          TDTag(
+            '警告',
+            isLight: true,
+            theme: TDTagTheme.warning,
+          ),
+          TDTag(
+            '危险',
+            isLight: true,
+            theme: TDTagTheme.danger,
+          ),
+          TDTag(
+            '成功',
+            isLight: true,
+            theme: TDTagTheme.success,
+          ),
+        ]),
+        Wrap(
+          spacing: 8,
+          children: const [
+            TDTag('默认'),
+            TDTag(
+              '主要',
+              theme: TDTagTheme.primary,
+            ),
+            TDTag(
+              '警告',
+              theme: TDTagTheme.warning,
+            ),
+            TDTag(
+              '危险',
+              theme: TDTagTheme.danger,
+            ),
+            TDTag(
+              '成功',
+              theme: TDTagTheme.success,
+            ),
+          ],
+        ),
+        Wrap(
+          spacing: 8,
+          children: const [
+            TDTag('默认', isOutline: true),
+            TDTag(
+              '主要',
+              isOutline: true,
+              theme: TDTagTheme.primary,
+            ),
+            TDTag(
+              '警告',
+              isOutline: true,
+              theme: TDTagTheme.warning,
+            ),
+            TDTag(
+              '危险',
+              isOutline: true,
+              theme: TDTagTheme.danger,
+            ),
+            TDTag(
+              '成功',
+              isOutline: true,
+              theme: TDTagTheme.success,
+            ),
+          ],
+        ),
+        Wrap(
+          spacing: 8,
+          children: const [
+            TDTag('默认', isOutline: true, isLight: true),
+            TDTag(
+              '主要',
+              isOutline: true,
+              isLight: true,
+              theme: TDTagTheme.primary,
+            ),
+            TDTag(
+              '警告',
+              isOutline: true,
+              isLight: true,
+              theme: TDTagTheme.warning,
+            ),
+            TDTag(
+              '危险',
+              isOutline: true,
+              isLight: true,
+              theme: TDTagTheme.danger,
+            ),
+            TDTag(
+              '成功',
+              isOutline: true,
+              isLight: true,
+              theme: TDTagTheme.success,
+            ),
+          ],
+        )
+      ]
     );
   }
 }
