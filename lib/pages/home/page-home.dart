2cbdd8cfe099c81b22b6ba6d902c9fd240195d5f
yangzhouQS <2638497222@qq.com>
2024-12-20 10:55:59 +0800

    fix: 本地图片加载实现

--- lib/pages/home/page-home.dart
+++ lib/pages/home/page-home.dart
@@ -1,9 +1,6 @@
-
-
 import 'package:flutter/material.dart';
 
-class PageHome  extends StatefulWidget {
-
+class PageHome extends StatefulWidget {
   const PageHome({Key? key}) : super(key: key);
 
   @override
@@ -15,6 +12,31 @@ class PageHome  extends StatefulWidget {
 class _PageHomeState extends State<PageHome> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Page Home'));
+    return Column(
+      children: <Widget>[
+        Flexible(
+          fit: FlexFit.tight,
+          child: Container(
+            padding: EdgeInsets.all(0),
+            color: Colors.blue,
+            child: Image(
+                image: AssetImage('assets/images/banner.jpg'),
+                fit: BoxFit.fitWidth,
+                width: double.infinity,
+                repeat: ImageRepeat.noRepeat,
+            )
+          )
+        ),
+        Flexible(
+          fit: FlexFit.tight,
+          flex: 2,
+          child: Container(
+            color: Colors.red,
+            height: 100,
+
+          )
+        ),
+      ],
+    );
   }
 }
